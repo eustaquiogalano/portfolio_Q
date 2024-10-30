b2e2d0bb074291c1158143e2605f91bdf4f4ad1e
eustaquio <eustaquio.galano@gmail.com>
2024-10-30 12:55:42 +0800

    Refactor: Update chat box style

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,16 @@
 const clickToChat = document.getElementById('chat');
 const chatBox = document.getElementById('chat-box'); 
-const chatBoxExitButton = document.querySelector('#chat-box > button');
+const chatBoxExitButton = document.querySelector('#top-chat-box > button');
 
 clickToChat.addEventListener("click", () => {
 
-    chatBox.style.display = "block";
-    chatBoxExitButton.style.display = "block";
+    chatBox.style.visibility = "visible";
 
 });
 
 chatBoxExitButton.addEventListener('click', () => {
 
-    chatBox.style.display = "none";
-    chatBoxExitButton.style.display = "none";
+    chatBox.style.visibility = "hidden";
 
 });
 
