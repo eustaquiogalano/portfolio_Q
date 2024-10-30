f7928bee5c5f02a81d13cb04e23b80187447923d
eustaquio <eustaquio.galano@gmail.com>
2024-10-30 11:45:31 +0800

    Refactor: Add functionality to exit the chat box

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-
 const clickToChat = document.getElementById('chat');
-//const selfIntroContainer = document.getElementById('self-introduction')
-const main = document.querySelector("main");
+const chatBox = document.getElementById('chat-box'); 
+const chatBoxExitButton = document.querySelector('#chat-box > button');
 
 clickToChat.addEventListener("click", () => {
-    
-    const chatBox = document.createElement("div");
-    chatBox.style.height = "400px";
-    chatBox.style.width = "350px"; 
-    chatBox.style.backgroundColor = "Gray";
-    chatBox.style.position = "fixed";
-    chatBox.style.left = "1550px"
-    chatBox.style.top = "500px"
 
-    main.appendChild(chatBox);
-    
+    chatBox.style.display = "block";
+    chatBoxExitButton.style.display = "block";
+
 });
+
+chatBoxExitButton.addEventListener('click', () => {
+
+    chatBox.style.display = "none";
+    chatBoxExitButton.style.display = "none";
+
+});
+
