6ee0b106c7f8e7ec1b20edb8ffdcb5e468663e94
eustaquio <eustaquio.galano@gmail.com>
2024-11-01 22:46:04 +0800

    Update variable names for clarity

--- projects/Shopping Website/case.js
+++ projects/Shopping Website/case.js
@@ -2,8 +2,8 @@ const minus = document.getElementById("minus");
 const plus = document.getElementById("plus");
 const quantityDisplay = document.getElementById("quantity-display");
 const priceDisplay = document.getElementById("price-display");
-const withinMM = document.getElementById("withinMM");
-const outsideMM = document.getElementById("outsideMM");
+const withinMetroManila = document.getElementById("withinMM");
+const outsideMetroManila = document.getElementById("outsideMM");
 let itemQuantity = 0;
 const itemPrice = 50;
 let totalPrice;
@@ -27,14 +27,13 @@ function subtractQuantity() {
     }
 };
 
-function shippingWithinMM() {
+function shippingWithinMetroManila() {
     priceDisplay.innerHTML = `$${totalPrice += 5}`;
 };
 
-function shippingOutsideMM() {
+function shippingOutsideMetroManila() {
     priceDisplay.innerHTML = `$${totalPrice += 10}`;
 };
-
 
 
 
@@ -42,5 +41,5 @@ function shippingOutsideMM() {
 
 plus.addEventListener("click", addQuantity);
 minus.addEventListener("click", subtractQuantity);
-withinMM.addEventListener("click", shippingWithinMM);
-outsideMM.addEventListener("click", shippingOutsideMM);
+withinMetroManila.addEventListener("click", shippingWithinMetroManila);
+outsideMetroManila.addEventListener("click", shippingOutsideMetroManila);
