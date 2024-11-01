1f0e98db4bfee40839621939fb09a5ca4976dd3a
eustaquio <eustaquio.galano@gmail.com>
2024-11-01 23:13:49 +0800

    Feat: Add search by ratings functionality

--- projects/Shopping Website/main.js
+++ projects/Shopping Website/main.js
@@ -2,9 +2,39 @@
 const searchBar = document.getElementById('search-bar');
 const searchButton = document.getElementById('search-button');
 const listOfAllProducts = document.querySelectorAll(`.item`);
-const initialItems = document.getElementById('initial-items');
+const initialItemsContainer = document.getElementById('initial-items-container'); 
+//const listOfRadioInputs = document.querySelector('#ratings-form #four-star'); 
+const fiveStarRate = document.querySelector('#five-star');
+const fourStarRate = document.querySelector('#four-star');
+const threeStarRate = document.querySelector('#three-star');
+const twoStarRate = document.querySelector('#two-star');
+const oneStarRate = document.querySelector('#one-star');
 
 
+////////////////////////////////////////////////////////////////////////////////////////
+
+function getCurrentDisplayedItems(rate) {
+
+  let generatedHTMLForDisplayingProducts = '';
+  const listOfCurrentProductsDisplayed = document.querySelectorAll('.item')
+
+  listOfCurrentProductsDisplayed.forEach(product => {
+    if (product.textContent.includes(`${rate} `)) {
+      generatedHTMLForDisplayingProducts += `
+      <div class="item">
+        ${product.innerHTML}
+      </div>
+    `;
+    }
+  })
+
+  initialItemsContainer.innerHTML = generatedHTMLForDisplayingProducts;
+  generatedHTMLForDisplayingProducts = '';
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
 
 searchButton.addEventListener('click', () => {
 
@@ -14,15 +44,36 @@ searchButton.addEventListener('click', () => {
 
     // check the text content of each element and its descendants for 
     // matching names inputted in the search bar 
-    if ( product.innerText.toLowerCase().includes(`${searchBar.value}`) ) {
+    if ( product.textContent.toLowerCase().includes(`${searchBar.value}`) ) {
       generatedHTMLForDisplayingProducts += `
         <div class="item">
           ${product.innerHTML}
         </div>
       `;
-    }
+    }   
   });
 
-  initialItems.innerHTML = generatedHTMLForDisplayingProducts;
+  initialItemsContainer.innerHTML = generatedHTMLForDisplayingProducts;
   generatedHTMLForDisplayingProducts = '';
+
+});
+
+fiveStarRate.addEventListener('click', () => {
+  getCurrentDisplayedItems(5);
+});
+
+fourStarRate.addEventListener('click', () => {
+  getCurrentDisplayedItems(4);
+});
+
+threeStarRate.addEventListener('click', () => {
+  getCurrentDisplayedItems(3);
+});
+
+twoStarRate.addEventListener('click', () => {
+  getCurrentDisplayedItems(2);
+});
+
+oneStarRate.addEventListener('click', () => {
+  getCurrentDisplayedItems(1);
 });
